9388a482c8777139c78bea6415595c24fdaea0af
SonamShukla10 <48238169+SonamShukla10@users.noreply.github.com>
2019-03-18 13:20:08 -0700

    Update logAnalysis.py

--- logAnalysis.py
+++ logAnalysis.py
@@ -8,27 +8,19 @@ DBNAME = "news"
 
 def execute_query(query):
     try:
-        
         db = psycopg2.connect(database=DBNAME)
-       
-        c = db.cursor()
-        
+        c = db.cursor()        
         c.execute(query)
-        
         results = c.fetchall()
-       
         db.close()
        
         return (results)
-
     except (Exception, psycopg2.DatabaseError) as error:
         print(error)
-
-
-
+        
+        
 def top_articles():
 
-    
     query = ("""
         SELECT title, count(title)
         FROM log, articles
@@ -38,19 +30,13 @@ def top_articles():
         LIMIT 3""")
     
     view_articles = run_query(query)
-
-    
-    print('\nWhat are the most popular three articles of all time?\n')
-
-   
+    print('\nWhat are the most popular three articles of all time?\n')   
     for title, views in view_articles:
         list = "  " + '"' + title + '"' + " - " + str(views) + " views\n"
         sys.stdout.write(list)
 
 
-def get_authors():
-
-   
+def get_authors():   
     query = ("""
         SELECT name, count(title)
         FROM log, articles, authors
@@ -58,19 +44,12 @@ def get_authors():
         AND authors.id = articles.author
         GROUP BY authors.name
         ORDER BY count DESC""")
-
-    
     view_authors = run_query(query)
-
-   
     print('\nWho are the most popular article authors of all time?\n')
-
-  
     for name, views in view_authors:
         print("  ", name, "-", views, "views")
-
-
-
+        
+        
 def get_errors():
 
     # SQL to retrieve error codes more than 1% (from 2 diff views)
@@ -86,26 +65,17 @@ def get_errors():
                pct_errs
         FROM t
         WHERE pct_errs > 1.0""")
-
-    
     err_days = run_query(query)
-
-    
     print("\nOn which days did more than 1% of requests lead to errors?\n")
-
-   
     for date, pct_errs in err_days:
         bad_status = "  " + date + " - " + str(pct_errs) + "% errors\n"
         sys.stdout.write(bad_status)
         print('\n')
 
-
-
 if __name__ == '__main__':
     top_articles()
-
     get_authors()
-
     get_errors()
+    
           
          
