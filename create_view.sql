25cc7f813a8ff78173cffbe911bc7c57ed3df7d0
Sonam Shukla <48238169+SonamShukla10@users.noreply.github.com>
2019-03-19 16:52:33 -0700

    Update create_view.sql

--- create_view.sql
+++ create_view.sql
@@ -2,14 +2,14 @@
 -- logs_analysis.py 
 
 CREATE VIEW err_reqs AS 
-SELECT time::date AS date, count(*) AS tot_err
+SELECT time::date AS date, count(*) AS total_err
 FROM log
 WHERE status <> '200 OK'
 GROUP BY date
 ORDER BY date ASC;
 
 
-CREATE VIEW tot_reqs AS 
+CREATE VIEW total_reqs AS 
 SELECT time::date AS date, count(*) AS totals
 FROM log
 GROUP BY date
